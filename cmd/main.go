e0c4c395ce4434c376d4ae0a1430a4a0005b38aa
arthur <arthur.wieb@gmail.com>
2024-04-14 12:38:42 -0300

    fim do ambiente ok para começar a zoar

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"context"
+
 	"github.com/arthurwieb/got/handler"
 	"github.com/labstack/echo/v4"
-	// https://echo.labstack.com/
 )
 
 type DB struct{}
 
 func main() {
-	// fmt.Println("Hello, World!")
 	app := echo.New()
+
 	userHandler := handler.UserHandler{}
+	app.Use(withUser)
 	app.GET("/user", userHandler.HandleUserShow)
 
 	app.Start(":3000")
+}
 
+func withUser(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := context.WithValue(c.Request().Context(), "user", "arthur@me.com")
+		c.SetRequest(c.Request().WithContext(ctx))
+		return next(c)
+	}
 }
--- handler/user.go
+++ handler/user.go
@@ -1,9 +1,16 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/arthurwieb/got/model"
+	"github.com/arthurwieb/got/view/user"
+	"github.com/labstack/echo/v4"
+)
 
 type UserHandler struct{}
 
 func (h UserHandler) HandleUserShow(c echo.Context) error {
-	return nil
+	u := model.User{
+		Email: "arthur@me.com",
+	}
+	return render(c, user.Show(u))
 }
